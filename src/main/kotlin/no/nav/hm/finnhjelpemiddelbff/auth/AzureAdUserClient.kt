b7f28e5f11ff64017ab974b092bfff77b3bed61e
marte pedersen <marte.pedersen@nav.no>
2026-01-08 16:21:53 +0100

    auth for kategori-redigering

---
+++ src/main/kotlin/no/nav/hm/finnhjelpemiddelbff/auth/AzureAdUserClient.kt
@@ -0,0 +1,28 @@
+package no.nav.hm.finnhjelpemiddelbff.auth
+
+import io.micronaut.core.async.annotation.SingleResult
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.serde.annotation.Serdeable
+
+@Client("\${azure.introspection.endpoint}")
+interface AzureAdUserClient {
+    @Post(value = "/", produces = [MediaType.APPLICATION_FORM_URLENCODED], consumes = ["application/json"])
+    @SingleResult
+    suspend fun validateToken(@Body authBody: AuthBody): AuthResponse
+
+}
+
+@Serdeable
+data class AuthBody(
+    val token: String,
+    val identity_provider: String? = "azuread",
+)
+
+@Serdeable
+data class AuthResponse(
+    val active: Boolean,
+    val error: String? = "aok"
+)
--- src/main/kotlin/no/nav/hm/finnhjelpemiddelbff/category/admin/CategoryAdminController.kt
+++ src/main/kotlin/no/nav/hm/finnhjelpemiddelbff/category/admin/CategoryAdminController.kt
@@ -3,8 +3,12 @@ package no.nav.hm.finnhjelpemiddelbff.category.admin
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Put
 import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.hm.finnhjelpemiddelbff.auth.AuthBody
+import no.nav.hm.finnhjelpemiddelbff.auth.AzureAdUserClient
 import no.nav.hm.finnhjelpemiddelbff.category.CategoryDto
 import no.nav.hm.finnhjelpemiddelbff.category.CategoryRepository
 import org.slf4j.LoggerFactory
@@ -12,27 +16,58 @@ import org.slf4j.LoggerFactory
 
 @Controller("/admin/category")
 @Tag(name = "Category administration")
-class CategoryAdminController(private val categoryRepository: CategoryRepository) {
+class CategoryAdminController(
+    private val categoryRepository: CategoryRepository,
+    private val azureAdUserClient: AzureAdUserClient
+) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(CategoryAdminController::class.java)
     }
 
     @Post("")
-    suspend fun createCategory(@Body categoryDto: CategoryDto): HttpResponse<String> = try {
-        categoryRepository.save(categoryDto)
-        HttpResponse.ok()
-    } catch (exception: Exception) {
-        LOG.error("Failed to create new category \"$categoryDto\"", exception)
-        HttpResponse.serverError()
+    suspend fun createCategory(
+        @Header("Authorization") authorization: String,
+        @Body categoryDto: CategoryDto
+    ): HttpResponse<String> {
+        val authToken = authorization.removePrefix("Bearer ")
+
+        val tokenValidated = azureAdUserClient.validateToken(AuthBody(token = authToken))
+
+        if (tokenValidated.active) {
+            try {
+                categoryRepository.save(categoryDto)
+                return HttpResponse.ok()
+            } catch (exception: Exception) {
+                LOG.error("Failed to create new category \"$categoryDto\"", exception)
+                return HttpResponse.serverError()
+            }
+        } else {
+            LOG.warn("Token fail: " + tokenValidated.error)
+            return HttpResponse.unauthorized()
+        }
     }
 
-    @Post("")
-    suspend fun updateCategory(@Body categoryDto: CategoryDto): HttpResponse<String> = try {
-        categoryRepository.update(categoryDto)
-        HttpResponse.ok()
-    } catch (exception: Exception) {
-        LOG.error("Failed to update category \"$categoryDto\"", exception)
-        HttpResponse.serverError()
+    @Put("")
+    suspend fun updateCategory(
+        @Header("Authorization") authorization: String,
+        @Body categoryDto: CategoryDto
+    ): HttpResponse<String> {
+        val authToken = authorization.removePrefix("Bearer ")
+
+        val tokenValidated = azureAdUserClient.validateToken(AuthBody(token = authToken))
+
+        if (tokenValidated.active) {
+            try {
+                categoryRepository.update(categoryDto)
+                return HttpResponse.ok()
+            } catch (exception: Exception) {
+                LOG.error("Failed to update category \"$categoryDto\"", exception)
+                return HttpResponse.serverError()
+            }
+        } else {
+            LOG.warn("Token fail: " + tokenValidated.error)
+            return HttpResponse.unauthorized()
+        }
     }
 }
